65f23c761e98d3a6930b86a72cf80feab4df1879
victorcruz23 <victor.cruz23@gmail.com>
2026-05-05 19:24:56 -0300

    adicionei novas entradas

--- aula010/exemplo03.py
+++ aula010/exemplo03.py
@@ -1,1 +1,24 @@
-print ("hello world")
+# for i in range (5):
+#     total_produzido = float(input("Digite o total produzido: "))
+#     funcionarios = int(input("Digite o numero de funcionarios: "))
+
+#     media_por_funcionarios = total_produzido / funcionarios
+#     print (f'Media por funcionario {media_por_funcionarios:.2f}')
+
+
+
+for i in range (5):
+
+    try:
+        total_produzido = float(input("Digite o total produzido: "))
+        funcionarios = int(input("Digite o numero de funcionarios: "))
+
+        media_por_funcionarios = total_produzido / funcionarios
+    
+    except ValueError:
+        print ("Informe um numero")
+    except ZeroDivisionError:
+        print ("Funcionarios nao pode ser zero")
+    else:   
+####### SE NAO DER ERRO, ELE VAI EXECUTAR O ELSE ####
+        print (f'Media por funcionario {media_por_funcionarios:.2f}')
---
+++ aula010/exemplo04.py
@@ -0,0 +1,28 @@
+# for i in range (5):
+#     total_produzido = float(input("Digite o total produzido: "))
+#     funcionarios = int(input("Digite o numero de funcionarios: "))
+
+#     media_por_funcionarios = total_produzido / funcionarios
+#     print (f'Media por funcionario {media_por_funcionarios:.2f}')
+
+# o finally sempre será executado e encerrará o programa
+
+# for i in range (5):
+
+#     try:
+#         total_produzido = float(input("Digite o total produzido: "))
+#         funcionarios = int(input("Digite o numero de funcionarios: "))
+
+#         media_por_funcionarios = total_produzido / funcionarios
+    
+#     except ValueError:
+#         print ("Informe um numero")
+#     except ZeroDivisionError:
+#         print ("Funcionarios nao pode ser zero")
+#     else:   
+# ####### SE NAO DER ERRO, ELE VAI EXECUTAR O ELSE ####
+#         print (f'Media por funcionario {media_por_funcionarios:.2f}')
+#     finally:
+#         print ("Programa encerrado")
+
+
